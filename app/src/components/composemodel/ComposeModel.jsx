2ea46ba88c233b7d25bff10a1974a270ed9ae32a
Victor Dibia <victor.dibia@gmail.com>
2019-11-07 08:49:55 -0500

    model composer encoder UI builder - implement layer/unit updates

--- app/src/components/composemodel/ComposeModel.jsx
+++ app/src/components/composemodel/ComposeModel.jsx
@@ -10,9 +10,9 @@ class ComposeModel extends Component {
         super(props)
 
         this.state = {
-            encoderDims: [8, 4, 3],
+            encoderDims: [8, 4],
             decoderDims: [7, 15],
-            latentDim: 2,
+            latentDim: [2],
             maxLayers: 6,
             minLayers: 2,
             maxUnits: 8,
@@ -58,8 +58,23 @@ class ComposeModel extends Component {
         }
     }
 
+    getDims(dimType) {
+        switch (dimType) {
+            case "encoder":
+                return this.state.encoderDims
+            case "decoder":
+                return this.state.decoderDims
+            case "latent":
+                return this.state.latentDim
+        }
+    }
+
+
+
+
     updateLayerClick(e) {
-        let currentDims = e.target.getAttribute("layergroup") === "encoder" ? this.state.encoderDims : this.state.decoderDims
+
+        let currentDims = this.getDims(e.target.getAttribute("layergroup"));
 
         if (e.target.getAttribute("buttonaction") === "add") {
 
@@ -74,13 +89,12 @@ class ComposeModel extends Component {
                 currentDims.pop()
                 this.setStateVal(e.target.getAttribute("layergroup"), currentDims)
             }
-
         }
     }
 
     updateUnits(e) {
 
-        let currentDims = e.target.getAttribute("layergroup") === "encoder" ? this.state.encoderDims : this.state.decoderDims
+        let currentDims = this.getDims(e.target.getAttribute("layergroup"));
         let currentUnit = currentDims[e.target.getAttribute("unitindex") * 1]
         // console.log(e.target.getAttribute("unitindex"), currentUnit);
         if (e.target.getAttribute("buttonaction") === "add") {
@@ -105,6 +119,43 @@ class ComposeModel extends Component {
 
     render() {
 
+        let latentLayers = this.state.latentDim.map((data, index) => {
+            let eachLayer = _.range(data).map((data, index) => {
+                return (
+                    <div className="eachlayerbox mb5" key={"eachlayer" + index}>
+                        {/* {index} */}
+                    </div>
+                )
+            })
+            return (
+                <div key={"latentlayer" + index} className="iblock  mr10 flex flexfull flexjustifycenter ">
+                    <div className="iblock  ">
+                        <div className="" >
+                            <div className="smalldesc mb3">{data} units</div>
+                            <div
+                                layergroup="latent"
+                                unitindex={index}
+                                buttonaction="add"
+                                onClick={this.updateUnits.bind(this)}
+                                className="updatebutton mb5 clickable">
+                                <Add16 className="unclickable"></Add16>
+                            </div>
+                            <div>{eachLayer}</div>
+                            <div
+                                layergroup="latent"
+                                unitindex={index}
+                                buttonaction="subtract"
+                                onClick={this.updateUnits.bind(this)}
+                                className="updatebutton  clickable">
+                                <Subtract16 className="unclickable"></Subtract16>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            )
+        })
+
         let encLayers = this.state.encoderDims.map((data, index) => {
             let eachLayer = _.range(data).map((data, index) => {
                 return (
@@ -114,10 +165,10 @@ class ComposeModel extends Component {
                 )
             })
             return (
-                <div key={"enclayer" + index} className="iblock mr10 flexfull ">
+                <div key={"enclayer" + index} className="iblock  mr10 flex flexfull flexjustifycenter ">
                     <div className="iblock  ">
                         <div>
-                            {/* <div>{data}</div> */}
+                            <div className="smalldesc mb3">{data} units</div>
                             <div
                                 layergroup="encoder"
                                 unitindex={index}
@@ -142,61 +193,50 @@ class ComposeModel extends Component {
             )
         })
 
+        // console.log(encLayers.length);
+        let decLayers = _.reverse(_.clone(encLayers));
+
         return (
             <div>
-                <div className="flex">
-                    <div className="encoder flex4 mr10">
-                        <div className="buttonbar mb10">
-                            <div
-                                layergroup="encoder"
-                                buttonaction="add"
-                                onClick={this.updateLayerClick.bind(this)}
-                                className="updatebutton mr5 clickable">
-                                <Add16 className="unclickable"></Add16>
-                            </div>
-                            <div
-                                layergroup="encoder"
-                                buttonaction="subtract"
-                                onClick={this.updateLayerClick.bind(this)}
-                                className="updatebutton mr5 clickable">
-                                <Subtract16 className="unclickable"></Subtract16>
-                            </div>
-                            {/* <Button
-                                buttonaction="add"
-                                className="mr5 iblock"
-                                onClick={this.addEncoderLayerClick.bind(this)}
-                                renderIcon={Add16}
-                                size="small"
-                                tooltipPosition="top"
-                                tooltipAlignment="start"
-                                iconDescription="+"
-                                // disabled={(!this.state.isTraining) ? false : true}
-                                hasIconOnly
-                            ></Button>
-                            <Button
-                                buttonaction="subtract"
-                                className="mr5 iblock"
-                                renderIcon={Subtract16}
-                                size="small"
-                                tooltipPosition="top"
-                                tooltipAlignment="start"
-                                iconDescription="-"
-                                onClick={this.removeEncoderLayerClick.bind(this)}
-                                // disabled={(!this.state.isTraining) ? false : true}
-                                hasIconOnly
-                            ></Button> */}
+                <div className="flex flexjustifycenter">
+                    <div className="buttonbar mb10 iblock">
+                        <div
+                            layergroup="encoder"
+                            buttonaction="add"
+                            onClick={this.updateLayerClick.bind(this)}
+                            className="updatebutton mr5 clickable">
+                            <Add16 className="unclickable"></Add16>
+                        </div>
+                        <div
+                            layergroup="encoder"
+                            buttonaction="subtract"
+                            onClick={this.updateLayerClick.bind(this)}
+                            className="updatebutton mr5 clickable">
+                            <Subtract16 className="unclickable"></Subtract16>
                         </div>
 
+                        <div className="ml10 iblock">
+                            {this.state.encoderDims.length} Layers
+                        </div>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div className="encoder flex4 mr10">
                         <div className="layerbar flex  flexjustifycenter">
                             {encLayers}
                         </div>
 
                     </div>
-                    <div className="bottlneck border flex2 mr10">
+                    <div className="bottlneck  flex2 mr10">
+                        <div className="layerbar   h100  flex  flexjustifycenter">
+                            {latentLayers}
 
+                        </div>
                     </div>
-                    <div className="decoder border flex4">
-
+                    <div className="decoder  flex4">
+                        <div className="layerbar flex  flexjustifycenter">
+                            {decLayers}
+                        </div>
                     </div>
 
                 </div>
