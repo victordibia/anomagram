5efeea54005664a1e7f3d63e1fff97e8c9b13eea
Victor Dibia <victor.dibia@gmail.com>
2019-11-20 14:13:58 -0500

    Updates

---
+++ precache-manifest.dcb390ed78e8f607ac82d79184e3ef3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f0b4b537d1781734f7abc0427e4577",
+    "url": "/anomagram/index.html"
+  },
+  {
+    "revision": "d84f1c3f3c33e45b153d",
+    "url": "/anomagram/static/css/main.becdb05b.chunk.css"
+  },
+  {
+    "revision": "89766206c1f59a932e17",
+    "url": "/anomagram/static/js/2.cce9cdf0.chunk.js"
+  },
+  {
+    "revision": "d84f1c3f3c33e45b153d",
+    "url": "/anomagram/static/js/main.e79ef144.chunk.js"
+  },
+  {
+    "revision": "f34ebbe55989ef8765ae",
+    "url": "/anomagram/static/js/runtime-main.7366a31c.js"
+  }
+]);
